2e29852afa1e8ade97e7eb5d834859ecf4de9d07
Jeroen Postema <jeroen@script.nl>
2020-10-06 12:56:23 +0200

    code review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+
+        // extra oefening: maak een GrocerySeeder zodat je bij het installeren van het project automatisch dummmy-data (boodschappen) kunt laten toevoegen
+        // voor meer informatie zie: https://laravel.com/docs/8.x/seeding
     }
 }
